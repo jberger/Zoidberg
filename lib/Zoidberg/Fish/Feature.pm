c64af84b1f5b2478610ab5ce66f551b6cef897a2
Joel Berger <joel.a.berger@gmail.com>
2011-12-05 15:29:04 -0600

    BEGIN strangeness

--- lib/Zoidberg/Fish/Feature.pm
+++ lib/Zoidberg/Fish/Feature.pm
@@ -9,10 +9,12 @@ use base 'Zoidberg::Fish';
 use version 0.77;
 
 # feature loading needs to happen at compile time, and as soon as possible.
-our $import_version = 0;
+our $import_version;
 our @feature_keywords;
 BEGIN {
   { # block for last-ing out
+    $import_version = 0;
+
     last if ($^V < v5.10.0);
 
     $import_version = ':5.10';
@@ -40,7 +42,7 @@ sub add_features {
   my $plugin = shift;
   if (@feature_keywords) {
     no strict 'refs';
-    my @commands = map { $_ => \*{ __PACKAGE__ . '::' . $_ } } @feature_keywords;
+    my @commands = map { $_ => \&{ __PACKAGE__ . '::' . $_ } } @feature_keywords;
     $plugin->add_commands(\@commands);
     message "Additional Perl features '$import_version' loaded\n\t'@feature_keywords' added as keywords.\n";
   }
