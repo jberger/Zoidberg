641b742f5d3d48c0742c99fc55795035cf8af075
Joel Berger <joel.a.berger@gmail.com>
2011-12-05 14:57:53 -0600

    A first attempt at adding features through the Fish system

---
+++ lib/Zoidberg/Fish/Feature.pm
@@ -0,0 +1,45 @@
+package Zoidberg::Fish::Feature;
+
+our $VERSION = '0.97';
+
+use strict;
+#use Zoidberg::Utils qw/:default path getopt output_is_captured/;
+use base 'Zoidberg::Fish';
+
+use version 0.77;
+
+# feature loading needs to happen at compile time, and as soon as possible.
+our @feature_keywords;
+BEGIN {
+  { # block for last-ing out
+    last if ($^V < v5.10.0);
+
+    require feature;
+
+    my $import_version = ':5.10';
+    push @_feature_keywords, qw'say state given';
+
+    if ( $_feature_version >= v5.12.0 ) {
+      $import_version = ':5.12'; 
+    }
+
+    if ( $_feature_version >= v5.14.0 ) {
+      $import_version = ':5.14'; # s///r
+    }
+
+    feature->import($import_version);
+    print STDERR "Additional Perl features '$import_version' loaded\n\t'@_feature_keywords' added as keywords.\n";
+
+  }
+}
+
+sub init {
+  my $plugin = shift;
+  $plugin->add_features();
+}
+
+sub add_features {
+  my $plugin = shift;
+  $plugin->add_commands(@feature_keywords) if @feature_keywords;
+}
+
---
+++ share/plugins/Feature.pl
@@ -0,0 +1,9 @@
+{
+	module  => 'Zoidberg::Fish::Feature',
+	#events  => { loadrc => 'plug' },
+	#aliases => {
+	#},
+	#export  => [qw/
+	#	say state given
+	#/],
+}
